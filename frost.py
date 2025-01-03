ab2df8716bc576c91e65ba19c5c774e65e224cd8
Woxez <139673932+Serpentsqw@users.noreply.github.com>
2025-01-03 03:59:07 +0300

    Update and rename enough.py to frost.py

--- frost.py
+++ frost.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from colorama import Fore, Style
 from time import sleep
 from os import system
@@ -15,16 +17,17 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
-    
-    Sms: {}           {}by {}@tingirifistik\n  
+█████▒██▀███   ▒█████    ██████ ▄▄▄█████▓
+▓██   ▒▓██ ▒ ██▒▒██▒  ██▒▒██    ▒ ▓  ██▒ ▓▒
+▒████ ░▓██ ░▄█ ▒▒██░  ██▒░ ▓██▄   ▒ ▓██░ ▒░
+░▓█▒  ░▒██▀▀█▄  ▒██   ██░  ▒   ██▒░ ▓██▓ ░ 
+░▒█░   ░██▓ ▒██▒░ ████▓▒░▒██████▒▒  ▒██▒ ░ 
+ ▒ ░   ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒ ▒▓▒ ▒ ░  ▒ ░░   
+ ░       ░▒ ░ ▒░  ░ ▒ ▒░ ░ ░▒  ░ ░    ░    
+ ░ ░     ░░   ░ ░ ░ ░ ▒  ░  ░  ░    ░      
+          ░         ░ ░        ░           
+                                           
+    Sms: {}           {}by {}@sovex.online\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
